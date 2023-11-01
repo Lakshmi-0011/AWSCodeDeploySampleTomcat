436c73763254e8eaeb2ccd938f4b97f84812d5c2
Lakshmi-0011 <131253198+Lakshmi-0011@users.noreply.github.com>
2023-11-01 18:21:15 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-aws s3 cp s3://java-artifacts-devops4solutions/target/LoginWebApp-1.war/sample-java-projects/LoginWebApp-1.war /tmp
+aws s3 cp s3://java-artifact/target/LoginWebApp-1.war/sample-java-projects/LoginWebApp-1.war /tmp
 sudo mv /tmp/LoginWebApp-1.war /usr/share/tomcat/webapps/LoginWebApp-1.war
 ./opt/tomcat/bin/shutdown.sh
 ./opt/tomcat/bin/startup.sh
